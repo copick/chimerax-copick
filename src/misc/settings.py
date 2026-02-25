da3b2a579d94ac0ff6733676c0b0ac6a775685f2
uermel <uermel@czii.org>
2026-02-24 18:42:53 -0800

    allow setting default zarr level to load.

--- src/misc/settings.py
+++ src/misc/settings.py
@@ -4,4 +4,6 @@ from chimerax.core.settings import Settings
 class CoPickSettings(Settings):
     EXPLICIT_SAVE = {}
 
-    AUTO_SAVE = {}
+    AUTO_SAVE = {
+        "zarr_level": 2,  # Preferred zarr pyramid level (0=full, 1=2x, 2=4x downsampled)
+    }
